248650c61b568aa122081fa786babbeadc6eb4fe
markwbrown <markbr.wn@gmail.com>
2016-11-19 12:07:27 -0800

    Update deletePrice.php

--- ajax/deletePrice.php
+++ ajax/deletePrice.php
@@ -6,9 +6,8 @@ $lastId = filter_var($_REQUEST['lastId'], FILTER_SANITIZE_STRING);
 $secondLastId = filter_var($_REQUEST['secondLastId'], FILTER_SANITIZE_STRING);
 $lastValue = filter_var($_REQUEST['lastValue'], FILTER_SANITIZE_STRING);
 
-$deletessql = "DELETE FROM :tbl_price WHERE id = :lastId";
+$deletessql = "DELETE FROM $TBL_PRICE WHERE id = :lastId";
 $deletestatement = $pdo->prepare($deletesql);
-$deletestatement->bindParam(':table_price', $TBL_PRICE);
 $deletestatement->bindParam(':id', $lastId);
 if(!$deletestatement->execute()){
       header("Status Code: 503 Server error");
@@ -16,9 +15,8 @@ if(!$deletestatement->execute()){
     }
 
 // Need to change the value of maxNo in the second last pricing for that product id
-$updatesql = "UPDATE :tbl_price SET high_num_pers =:lastValue WHERE id=:secondLastId";
+$updatesql = "UPDATE $TBL_PRICE SET high_num_pers =:lastValue WHERE id=:secondLastId";
 $updatestatement = $pdo->prepare($updatesql);
-$updatestatement->bindParam(':tbl_price', $TBL_PRICE);
 $updatestatement->bindParam(':lastValue', $lastValue);
 $updatestatement->bindParam(':id', $secondLastId);
 if(!$updatestatement->execute()){
