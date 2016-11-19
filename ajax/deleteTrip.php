4e7688bb6936d24d813e6a4994b9061a31d48bdd
markwbrown <markbr.wn@gmail.com>
2016-11-19 12:08:15 -0800

    Update deleteTrip.php

--- ajax/deleteTrip.php
+++ ajax/deleteTrip.php
@@ -3,9 +3,8 @@
 require_once '../includes/db.php';
 
 $product_id = filter_var($_REQUEST['pid'], FILTER_SANITIZE_STRING);
-$deletessql = "DELETE FROM :tbl_prod WHERE id = :product_id";
+$deletessql = "DELETE FROM $TBL_PROD WHERE id = :product_id";
 $deletestatement = $pdo->prepare($deletesql);
-$deletestatement->bindParam(':tbl_prod', $TBL_PROD);
 $deletestatement->bindParam(':product_id', $product_id);
 if(!$deletestatement->execute()){
       header("Status Code: 503 Server error");
@@ -13,9 +12,8 @@ if(!$deletestatement->execute()){
     }
 
 //Also delete the associated prices
-$deletessql2 = "DELETE FROM :tbl_price WHERE id = :product_id";
+$deletessql2 = "DELETE FROM $TBL_PRICE WHERE id = :product_id";
 $deletestatement2 = $pdo->prepare($deletesql);
-$deletestatement2->bindParam(':tbl_price', $TBL_PRICE);
 $deletestatement2->bindParam(':product_id', $product_id);
 
 if(!$deletestatement2->execute()){
