ba47689e31f92ca76e2aa177b48a468eafa06498
markwbrown <markbr.wn@gmail.com>
2016-11-19 10:32:11 -0800

    Update deletePrice.php
    filter vars, PDO

--- ajax/deletePrice.php
+++ ajax/deletePrice.php
@@ -2,22 +2,29 @@
 
 require_once '../includes/db.php';
 
-$lastId = $_REQUEST['lastId'];
-$secondLastId = $_REQUEST['secondLastId'];
-$lastValue = $_REQUEST['lastValue'];
+$lastId = filter_var($_REQUEST['lastId'], FILTER_SANITIZE_STRING);
+$secondLastId = filter_var($_REQUEST['secondLastId'], FILTER_SANITIZE_STRING);
+$lastValue = filter_var($_REQUEST['lastValue'], FILTER_SANITIZE_STRING);
 
-$query = "DELETE FROM  $TBL_PRICE WHERE id = $lastId";
-
-if (!$mysqli->query($query)) {
-  header("Status Code: 503 Server error");
-  exit;
-}
+$deletessql = "DELETE FROM :tbl_price WHERE id = :lastId";
+$deletestatement = $pdo->prepare($deletesql);
+$deletestatement->bindParam(':table_price', $TBL_PRICE);
+$deletestatement->bindParam(':id', $lastId);
+if(!$deletestatement->execute()){
+      header("Status Code: 503 Server error");
+      exit;
+    }
 
 // Need to change the value of maxNo in the second last pricing for that product id
-$query = "UPDATE $TBL_PRICE SET high_num_pers=$lastValue WHERE id=$secondLastId";
-if ($mysqli->query($query)) {
-  header("Status Code: 503 Server error");
-  exit;
-}
+$updatesql = "UPDATE :tbl_price SET high_num_pers =:lastValue WHERE id=:secondLastId";
+$updatestatement = $pdo->prepare($updatesql);
+$updatestatement->bindParam(':tbl_price', $TBL_PRICE);
+$updatestatement->bindParam(':lastValue', $lastValue);
+$updatestatement->bindParam(':id', $secondLastId);
+if(!$updatestatement->execute()){
+      header("Status Code: 503 Server error");
+      exit;
+    }
 
 echo "OK";
+?>
