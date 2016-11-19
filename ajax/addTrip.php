f87f3dcf63dacf2f27c3cfc41d958efd1041d2c3
markwbrown <markbr.wn@gmail.com>
2016-11-19 10:18:58 -0800

    Update addTrip.php

--- ajax/addTrip.php
+++ ajax/addTrip.php
@@ -3,25 +3,38 @@
 require_once '../includes/db.php';
 
 if (isset($_REQUEST['trip']) && isset($_GET['perc'])) {
-
-  $name = $_REQUEST['trip'];
-  $perc = $_REQUEST['perc'];
+  $name = filter_var($_REQUEST['trip'], FILTER_SANITIZE_STRING);
+  $perc = filter_var($_REQUEST['perc'], FILTER_SANITIZE_STRING);
 
   if (isset($_REQUEST['id'])) {
-    $updateId = $_REQUEST['id'];
-    $query = "UPDATE $TBL_PROD SET name='$name', commission_pct=$perc WHERE id=$updateId";
-//    echo $query;
+    $updateId = filter_var($_REQUEST['id'], FILTER_SANITIZE_STRING);
+    $updatesql = "UPDATE :table_prod SET name= :name, commission_pct :perc WHERE id= :updateId";
+    $updatestatement = $pdo->prepare($updatesql);
+    $updatestatement->bindParam(':table_prod', $_TBL_PROD);
+    $updatestatement->bindParam(':name', $name);
+    $updatestatement->bindParam(':commission_pct', $perc);
+    $updatestatement->bindParam(':id', $updateId);
+    if(!$updatestatement->execute()){
+      header("Status: 500");
+      echo 'Cannot update entry due to database error.' . print_r($pdo->errorInfo());
+      exit;
+    }
+    // Yayy!
+    echo $price_id;
   } else {
-    $query = "INSERT INTO $TBL_PROD(name, commission_pct) VALUES ('$name', $perc)";
+    $insertsql = "INSERT INTO :tbl_prod(name, comission_pct) VALUES(:name, :perc)";
+    $insertstatement = $pdo->prepare($insertsql);
+    $insertstatement->bindParam(':tbl_prod', $TBL_PROD);
+    $insertstatement->bindParam(':name', $name);
+    $insertstatement->bindParam(':perc', $perc);
+    if (!$insertstmt->execute()) {
+          header("Status: 500 Database error");
+          echo 'Cannot insert new item due to database errors.' . print_r($pdo->errorInfo());
+          exit;
+        }
+    // Cool!
+    echo $pdo->lastInsertId();
   }
-  if (!$mysqli->query($query)) {
-    header("Status: 500 Database error");
-    echo 'Cannot connect to database ' . $mysqli->error;
-    exit;
-  }
-
-  // Yayy!!
-  echo mysqli_insert_id($mysqli);
 } else {
   header("Status: 400 Incomplete data");
   echo "Not all fields are filled.";
