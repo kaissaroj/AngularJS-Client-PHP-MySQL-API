be3ed68877ec57038e3405371dcf28d413fb9ae4
markwbrown <markbr.wn@gmail.com>
2016-11-18 22:39:53 -0800

    update addPrice.php to use PDO & sanitize inputs

--- ajax/addPrice.php
+++ ajax/addPrice.php
@@ -1,31 +1,39 @@
 <?php
-
 require_once '../includes/db.php';
+$charset = "utf8";
+$dsn ="mysql:host=$DB_HOST;dbname=$DB_NAME;charset=$charset";
+$opt = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+try{
+$pdo = new PDO($dsn, $DB_USER, $DB_PASS, $opt);
 
 if ($_REQUEST['productId'] || $_REQUEST['minNo'] || $_REQUEST['maxNo'] || $_REQUEST['retail']) {
-
-  $low_num_pers = $_REQUEST['minNo'];
-  $high_num_pers = $_REQUEST['maxNo'];
-  $retail_rate_per_pers = $_REQUEST['retail'];
-  $product_id = $_REQUEST['productId'];
-
+  $low_num_pers = filter_var($_REQUEST['minNo'], FILTER_SANITIZE_STRING);
+  $high_num_pers = filter_var($_REQUEST['maxNo'], FILTER_SANITIZE_STRING);
+  $retail_rate_per_pers = filter_var($_REQUEST['retail'], FILTER_SANITIZE_STRING);
+  $product_id = filter_var($_REQUEST['productId'], FILTER_SANITIZE_STRING);
+}
   if (!$high_num_pers) {
     $high_num_pers = 0;
   }
 
-
-
   // (Optional) Is it an update?
   if (isset($_REQUEST['priceId'])) {
-    $price_id = $_REQUEST['priceId'];
-
-    $query = "UPDATE $TBL_PRICE SET low_num_pers = $low_num_pers,"
-            . " high_num_pers = $high_num_pers, retail_rate_per_pers = $retail_rate_per_pers"
-            . " ,product_id = $product_id WHERE id = $price_id";
-
-    if (!$mysqli->query($query)) {
+    $price_id = filter_var($_REQUEST['priceId'], FILTER_SANITIZE_STRING);
+    $updatesql = "UPDATE :table_price SET low_num_pers= :low_num_pers, high_num_pers= :high_num_pers, retail_rate_per_pers=:retail_rate_per_pers, product_id=:product_id WHERE id= :price_id";
+    $updatestatement = $pdo->prepare($updatesql);
+    $updatestatement->bindParam(':table_price', $_TBL_PRICE);
+    $updatestatement->bindParam(':low_num_pers', $low_num_pers);
+    $updatestatement->bindParam(':high_num_pers', $high_num_pers);
+    $updatestatement->bindParam(':retail_rate_per_pers', $retail_rate_per_pers);
+    $updatestatement->bindParam(':product_id', $product_id);
+    $updatestatement->bindParam(':price_id', $price_id);
+    if(!$updatestatement->execute()){
       header("Status: 500");
-      echo 'Cannot update entry due to database error.' . $mysqli->error;
+      echo 'Cannot update entry due to database error.' . print_r($pdo->errorInfo());
       exit;
     }
     // Yayy!
@@ -33,17 +41,23 @@ if ($_REQUEST['productId'] || $_REQUEST['minNo'] || $_REQUEST['maxNo'] || $_REQU
   }
   // No, this is a new entry
   else {
-    $query = "INSERT INTO tbl_product_price(low_num_pers,high_num_pers,retail_rate_per_pers,product_id)"
-            . " VALUES ('$low_num_pers','$high_num_pers','$retail_rate_per_pers','$product_id')";
-
-    if (!$mysqli->query($query)) {
+    $insert = "INSERT INTO tbl_product_price(low_num_pers, high_num_pers, retail_rate_per_pers, product_id) VALUES(:low_num_pers, :high_num_pers, :retail_rate_per_pers, :product_id)";
+    $insertstmt = $pdo->prepare($insert);
+    $insertstmt->bindParam(':low_num_pers', $low_num_pers);
+    $insertstmt->bindParam(':high_num_pers', $high_num_pers);
+    $insertstmt->bindParam(':retail_rate_per_pers', $retail_rate_per_pers);
+    $insertstmt->bindParam(':product_id', $product_id);
+    if (!$insertstmt->execute()) {
       header("Status: 500 Database error");
-      echo 'Cannot insert new item due to database errors.' . $mysqli->error;
+      echo 'Cannot insert new item due to database errors.' . print_r($pdo->errorInfo());
       exit;
     }
     // Cool!
-    echo mysqli_insert_id($mysqli);
+    echo $pdo->lastInsertId();
   }
-} else {
-  header("Status: 500");
+}     catch(Exception $e){
+    header("Status: 500");
+    echo 'Cannot insert new item due to database errors.'. $e->getMessage();
+     
 }
+?>
