30110936548ed8feeb155b2b979b8fdb7a28cbf4
markwbrown <markbr.wn@gmail.com>
2016-11-19 10:37:11 -0800

    update deleteTrip.php
    filter vars, use PDO

--- ajax/deleteTrip.php
+++ ajax/deleteTrip.php
@@ -2,20 +2,26 @@
 
 require_once '../includes/db.php';
 
-$product_id = $_REQUEST['pid'];
-$query = "DELETE FROM $TBL_PROD WHERE id=$product_id";
-
-if (!$mysqli->query($query)) {
-  header("Status Code: 503 Server error");
-  exit;
-}
+$product_id = filter_var($_REQUEST['pid'], FILTER_SANITIZE_STRING);
+$deletessql = "DELETE FROM :tbl_prod WHERE id = :product_id";
+$deletestatement = $pdo->prepare($deletesql);
+$deletestatement->bindParam(':tbl_prod', $TBL_PROD);
+$deletestatement->bindParam(':product_id', $product_id);
+if(!$deletestatement->execute()){
+      header("Status Code: 503 Server error");
+      exit;
+    }
 
 //Also delete the associated prices
-$query = "DELETE FROM $TBL_PRICE WHERE product_id=$product_id";
+$deletessql2 = "DELETE FROM :tbl_price WHERE id = :product_id";
+$deletestatement2 = $pdo->prepare($deletesql);
+$deletestatement2->bindParam(':tbl_price', $TBL_PRICE);
+$deletestatement2->bindParam(':product_id', $product_id);
 
-if (!$mysqli->query($query)) {
-  header("Status Code: 503 Server error");
-  exit;
-}
+if(!$deletestatement2->execute()){
+      header("Status Code: 503 Server error");
+      exit;
+    }
 
 echo "OK";
+?>
